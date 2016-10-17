5bda44498351691068f04c9b4463b2d816f33d6e
Marco <macrozone@gmail.com>
2016-10-18 00:59:28 +0200

    improve export /import

--- admin_module.js
+++ admin_module.js
@@ -5,12 +5,14 @@ import AdminMethods from './methods';
 import Routes from './routes';
 import routeUtils from './route_utils';
 import Papa from 'papaparse';
+import flat from 'flat';
+
 export default (adminConfig) => {
   const routes = Routes(adminConfig);
   const methods = AdminMethods(adminConfig);
   const actions = {
     manulAdmin: {
-      update({adminContext: {gotoRoute, Methods, showError = _.noop, showSuccess = _.noop}}, collectionName, doc) {
+      update({ adminContext: { gotoRoute, Methods, showError = _.noop, showSuccess = _.noop } }, collectionName, doc) {
         methods[collectionName].update.call(doc, (error) => {
           if (error) {
             showError(error);
@@ -21,10 +23,9 @@ export default (adminConfig) => {
             showSuccess('Update successfull');
             gotoRoute(routeUtils.getListRoute(collectionName).name);
           }
-
         });
       },
-      create({adminContext: {gotoRoute, Methods, showError = _.noop, showSuccess = _.noop}}, collectionName, doc) {
+      create({ adminContext: { gotoRoute, Methods, showError = _.noop, showSuccess = _.noop } }, collectionName, doc) {
         methods[collectionName].create.call(doc, (error, _id) => {
           if (error) {
             showError(error);
@@ -33,38 +34,79 @@ export default (adminConfig) => {
               adminConfig.hooks[collectionName].postCreate({ Methods }, _id);
             }
             showSuccess('Create successfull');
-            gotoRoute(routeUtils.getEditRoute(collectionName).name, {_id});
+            gotoRoute(routeUtils.getEditRoute(collectionName).name, { _id });
           }
-
         });
       },
-      downloadCsv({adminContext: {showError = _.noop}}, collectionName) {
-        methods[collectionName].export.call({}, (error, data) => {
+      downloadCsv({ adminContext: { showError = _.noop } }, collectionName) {
+        methods[collectionName].export.call({}, (error, { data, keys }) => {
           if (error) {
             showError(error);
           } else {
-            const csv = Papa.unparse(data);
-            const blob = new Blob([ csv ]);
+            // console.log(data, keys);
+            // we encode missing values with "NULL"
+            // because CSV has no concept of null/missing values
+            // good read: http://www.garretwilson.com/blog/2009/04/23/csvnull.xhtml
+            const defaults = _.zipObject(keys, keys.map(() => 'NULL'));
+            // console.log(defaults);
+            const dataPadded = data.map(entry => ({ ...defaults, ...entry }));
+            // console.log(dataPadded);
+            const csv = Papa.unparse(dataPadded);
+            const blob = new Blob([csv]);
             const a = window.document.createElement('a');
-            a.href = window.URL.createObjectURL(blob, {type: 'text/plain'});
+            a.href = window.URL.createObjectURL(blob, { type: 'text/plain' });
             a.download = `export_${collectionName}.csv`;
             document.body.appendChild(a);
             a.click();
             document.body.removeChild(a);
           }
         });
-      }
-    }
-  }
-  const load = ({adminContext}) => {
+      },
+      importCsv({ adminContext }, { collectionName, file, onInsert = _.noop, onUpdate = _.noop, onComplete = _.noop }) {
+        let counter = -1;
+        const imported = new Set();
+        Papa.parse(file, {
+          header: true,
+          dynamicTyping: true,
+          complete({ data }) {
+            data.forEach((entryUncleaned) => {
+              counter += 1;
+              const index = counter; // need constant closure copy
+              const checkForComplete = () => {
+                imported.add(index);
+                if (imported.size === data.length) {
+                  onComplete();
+                }
+              };
+              // console.log('uncleaned', entryUncleaned);
+              const entry = flat.unflatten(
+                _.omitBy(entryUncleaned, value => value === 'NULL')
+              );
+              // console.log('cleaned', entry);
+              if (entry._id) {
+                methods[collectionName].update.call(entry, (error) => {
+                  onUpdate(index, error, entry);
+                  checkForComplete();
+                });
+              } else {
+                delete entry._id; // if falsy
+                methods[collectionName].create.call(entry, (error, _id) => {
+                  onInsert(index, error, { _id, ...entry });
+                  checkForComplete();
+                });
+              }
+            });
+          },
+        });
+      },
+    },
+  };
+  const load = ({ adminContext }) => {
     if (!adminContext) {
       throw new Error('Please provide a adminContext-object in your mantra-context');
     }
-
-  }
-
+  };
 
 
-  return {routes, actions, load}
-
-}
+  return { routes, actions, load };
+};
--- methods.js
+++ methods.js
@@ -1,34 +1,39 @@
-import {ValidatedMethod} from 'meteor/mdg:validated-method';
-import {SimpleSchema} from 'meteor/aldeed:simple-schema';
+import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import IsAllowed from './is_allowed';
-import {Meteor} from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import flatten from 'flat';
 import _ from 'lodash';
 
 export default (adminConfig) => {
   const isAllowed = IsAllowed(adminConfig);
   const createFor = (collectionName) => {
-    const {collection} = adminConfig.collections[collectionName];
+    const { collection } = adminConfig.collections[collectionName];
     return {
       update: new ValidatedMethod({
         name: `manulAdmin.${collectionName}.update`,
-        validate: new SimpleSchema([ collection.simpleSchema(), {_id: {type: String}} ]).validator(),
-        run({_id, ...doc}) {
+        validate: new SimpleSchema([collection.simpleSchema(), { _id: { type: String } }]).validator({ clean: true }),
+        run({ _id, ...doc }) {
+          // console.log('updating', collectionName, _id, doc);
           if (!isAllowed(collectionName, this.userId)) {
             throw new Meteor.Error('not allowed', 'You are not allowed');
           }
-          return collection.update(_id, {$set: doc});
-        }
+          const updated = collection.update(_id, { $set: doc });
+          if (updated === 0) {
+            throw new Meteor.Error('not found', 'Entry not found');
+          }
+        },
       }),
       create: new ValidatedMethod({
         name: `manulAdmin.${collectionName}.create`,
-        validate: collection.simpleSchema().validator(),
+        validate: collection.simpleSchema().validator({ clean: true }),
         run(doc) {
+          // console.log('inserting', doc);
           if (!isAllowed(collectionName, this.userId)) {
             throw new Meteor.Error('not allowed', 'You are not allowed');
           }
           return collection.insert(doc);
-        }
+        },
       }),
       export: new ValidatedMethod({
         name: `manulAdmin.${collectionName}.export`,
@@ -41,23 +46,28 @@ export default (adminConfig) => {
             }
 
             // empty objects like {} are preserved by flat, but we like to have them empty (null)
-            const isEmptyObject = (field) => _.isObject(field) && _.isEmpty(field)
-            const removeEmptyObjects = (doc) => _.omitBy(doc, isEmptyObject);
+            const isEmptyObject = field => _.isObject(field) && !_.isDate(field) && _.isEmpty(field);
+            const removeEmptyObjects = doc => _.omitBy(doc, isEmptyObject);
+
             // TODO: use schema to define keys
-            return collection.find().map(flatten).map(removeEmptyObjects)
+
+            const data = collection.find().map(flatten).map(removeEmptyObjects);
+            const keysSet = new Set();
+            data.forEach(entry => _.keys(entry).forEach(key => keysSet.add(key)));
+            return {
+              data, keys: [...keysSet.values()],
+            };
           }
-        }
+        },
       }),
 
-    }
-  }
+    };
+  };
 
   const methods = {
-  }
+  };
   Object.keys(adminConfig.collections).forEach((collectionName) => {
     methods[collectionName] = createFor(collectionName);
-  })
+  });
   return methods;
-
-
-}
+};
