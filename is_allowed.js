5322fb4e87a3549fbf00c27e1ab1ae7003b9270b
Claudio Romano <claudiocro@gmail.com>
2016-10-18 16:28:37 +0200

    feat(admin): allow custom editor to be defined

--- is_allowed.js
+++ is_allowed.js
@@ -1,10 +1,9 @@
 import _ from 'lodash';
 
 export default (adminConfig) => {
-  const {collections, allowRules: globalAllowRules = []} = adminConfig;
+  const { collections, allowRules: globalAllowRules = [] } = adminConfig;
   return (collectionName, userId) => {
-
-    const {allowRules = []} = collections[collectionName];
+    const { allowRules = [] } = collections[collectionName];
     const rules = globalAllowRules.concat(allowRules);
     return _.some(rules, allowed => allowed(userId));
   };
--- routes.js
+++ routes.js
@@ -1,42 +1,51 @@
 import routeUtils from './route_utils';
 import publicationUtils from './publication_utils';
 import { mount } from 'react-mounter';
+import _ from 'lodash';
 
 export default (adminConfig) => {
-  const getProps = ({gotoRoute}, name) => {
+  const getProps = ({ gotoRoute }, name) => {
     const publications = publicationUtils.getPublications(name);
-    const {collection, schema, ...colConfig} = adminConfig.collections[name];
+    const { collection, schema, ...colConfig } = adminConfig.collections[name];
 
     return {
       collectionName: name,
       gotoCreate: () => gotoRoute(routeUtils.getCreateRoute(name).name),
-      gotoEdit: (_id) => gotoRoute(routeUtils.getEditRoute(name).name, {_id}),
+      gotoEdit: (_id) => gotoRoute(routeUtils.getEditRoute(name).name, { _id }),
       gotoList: () => gotoRoute(routeUtils.getListRoute(name).name),
       collection,
       schema: schema || collection.simpleSchema(),
       colConfig,
-      publications
+      publications,
     };
-  }
-  const routes = (injectDeps, {adminContext}) => {
-    const {adminRoutes, components} = adminContext;
+  };
+  const routes = (injectDeps, { adminContext }) => {
+    const { adminRoutes, components } = adminContext;
     for (const collectionName of Object.keys(adminConfig.collections)) {
-      for (const type of [ 'create', 'list', 'edit' ]) {
-
-        const {path, name} = routeUtils.getRoute(type, collectionName);
+      for (const type of ['create', 'list', 'edit']) {
+        const { path, name } = routeUtils.getRoute(type, collectionName);
         const props = getProps(adminContext, collectionName);
-        const Component = components[type];
+        let Component;
+
+        if (_.isFunction(components[type])) {
+          Component = components[type];
+        } else if (_.has(components, `${type}.${collectionName}`)) {
+          Component = components[type][collectionName];
+        } else {
+          Component = components[type].default;
+        }
+
         adminRoutes.route(path, {
           name,
           action(params) {
             mount(injectDeps(components.layout), {
-              content: () => (<Component {...props} params={params} />)
-            })
-          }
+              content: () => (<Component {...props} params={params} />),
+            });
+          },
         });
       }
     }
-  }
+  };
 
   return routes;
-}
+};
