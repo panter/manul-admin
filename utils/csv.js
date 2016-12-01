0a23027fe941da4b83888e5988bce1bbabc88488
Claudio Romano <claudiocro@gmail.com>
2016-12-01 14:23:58 +0100

    feat(csv): parseOptions and ability to pass column titles

--- utils/csv.js
+++ utils/csv.js
@@ -1,13 +1,15 @@
 import _ from 'lodash';
 import Papa from 'papaparse';
+import { T } from '/manul-i18n';
 
-const exportAsCsv = ({ filename, keys, data }) => {
+const exportAsCsv = ({ filename, keys, columnTitles, data, parseOptions = { delimiter: ';' } }) => {
   // we encode missing values with "NULL"
   // because CSV has no concept of null/missing values
   // good read: http://www.garretwilson.com/blog/2009/04/23/csvnull.xhtml
   const defaults = _.zipObject(keys, keys.map(() => 'NULL'));
-  const dataPadded = data.map(entry => ({ ...defaults, ...entry }));
-  const csv = Papa.unparse(dataPadded);
+  const columns = columnTitles || keys;
+  const dataPadded = data.map(entry => (_.values({ ...defaults, ...entry })));
+  const csv = Papa.unparse({ fields: columns, data: dataPadded }, parseOptions);
   const blob = new Blob([csv]);
   const a = window.document.createElement('a');
   a.href = window.URL.createObjectURL(blob, { type: 'text/plain' });
