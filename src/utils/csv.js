7cb17331b80f9d71ed1f71ad6036eba1b5dc2822
Marco <macrozone@gmail.com>
2017-03-01 16:32:43 +0100

    changed options

--- src/utils/csv.js
+++ src/utils/csv.js
@@ -10,17 +10,29 @@ keys: all keys of every document that should be included
 filename: the filename of the resulting csv-file
 columnTitles: the column titles on the first row
 useBom: whether to include a UTF-16 byte order mark
-parseOptions: options for papaparse
+delimiter: the delimiter for the csv
+quotes: whether to add quotes around fields
+any additional property will be passed to papaparse, see http://papaparse.com/docs#json-to-csv
 **/
-const exportAsCsv = ({ filename, keys, columnTitles, data, useBom = false, parseOptions = { delimiter: ';' }, nullValue = 'NULL' }) => {
+const exportAsCsv = ({
+  filename,
+  keys,
+  columnTitles,
+  data,
+  useBom = false,
+  delimiter = ';',
+  quotes = true,
+  nullValue = 'NULL',
+  ...additionalProps
+ }) => {
   // we encode missing values with "NULL"
   // because CSV has no concept of null/missing values
   // good read: http://www.garretwilson.com/blog/2009/04/23/csvnull.xhtml
   const defaults = _.zipObject(keys, keys.map(() => nullValue));
   const columns = columnTitles || keys;
   const dataPadded = data.map(entry => (_.values({ ...defaults, ...entry })));
-
-  const csv = Papa.unparse({ fields: columns, data: dataPadded }, parseOptions);
+  const papaOptions = { delimiter, quotes, ...additionalProps };
+  const csv = Papa.unparse({ fields: columns, data: dataPadded }, papaOptions);
   saveAs(new window.Blob([csv], { type: 'text/plain;charset=utf-8' }), `${filename}.csv`, useBom);
 };
 
